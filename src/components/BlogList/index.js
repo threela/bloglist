638f795ca2593b89b254d40e3ece869778962be5
threela <threelagonthireddy@gmail.com>
2023-06-04 09:27:32 +0000

    solution

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+// Write your JS code here
+import BlogItem from '../BlogItem'
+import './index.css'
+
+const BlogList = props => {
+  const {blogsList} = props
+
+  return (
+    <ul className="blog-list-container">
+      {blogsList.map(eachItem => (
+        <BlogItem key={eachItem.id} blogDetails={eachItem} />
+      ))}
+    </ul>
+  )
+}
+
+export default BlogList
